53e63c120d2b4310481e33b28746dcf98ebf2f21
devin <h1051211002@student.untan.ac.id>
2024-08-27 13:23:38 +0700

    modified table

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -22,14 +22,14 @@
                     <div class="col-lg-10">
                         <div class="row justify-content-end mt-3 mb-4">
                             <div class="col-lg-8 text-end">
-                                <a href="{{ route('add.cam') }}" class="btn btn-success">Add New</a>
+                                <a href="{{ route('add.cam') }}" class="btn btn-success btn-sm">Add New</a>
                             </div>
                         </div>
                         <div class="table-responsive">
-                            <table class="table table-bordered table-striped">
+                            <table class="table table-bordered table-striped table-sm">
                                 <thead class="table-dark">
-                                    <tr>
-                                        <th>Nomor</th>
+                                    <tr class="text-center">
+                                        <th>No</th>
                                         <th>Nama</th>
                                         <th>Merk</th>
                                         <th>Pemakai</th>
@@ -40,20 +40,18 @@
                                 <tbody>
                                     @foreach ($inventories as $index => $inventory)
                                         <tr>
-                                            <td>{{ $index + 1 }}</td>
+                                            <td class="text-center">{{ $index + 1 }}</td>
                                             <td>{{ $inventory->name }}</td>
                                             <td>{{ $inventory->brand }}</td>
                                             <td>{{ $inventory->user }}</td>
                                             <td>{{ $inventory->year }}</td>
-                                            <td>
-                                                <a href="{{ route('edit.page', ['id' => $inventory->id]) }}"
-                                                    class="btn btn-primary btn-sm">Edit</a>
+                                            <td class="text-center">
+                                                <a href="{{ route('edit.page', ['id' => $inventory->id]) }}" class="btn btn-primary btn-sm">Edit</a>
                                                 <form action="{{ route('delete.process', ['id' => $inventory->id]) }}"
                                                     method="POST" style="display:inline;">
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm"
-                                                        onclick="return confirm('Are you sure you want to delete this item?');">Delete</button>
+                                                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this item?');">Delete</button>
                                                 </form>
                                             </td>
                                         </tr>
