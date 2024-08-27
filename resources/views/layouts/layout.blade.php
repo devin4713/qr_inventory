9368a32c944ee4e21272f73e57b54d2b99bad888
devin <h1051211002@student.untan.ac.id>
2024-08-27 16:29:20 +0700

    changed mobile app icon

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -10,7 +10,7 @@
 
     <!-- Favicons -->
     <link href={{ asset('assets/img/invlogo.png') }} rel="icon">
-    <link href={{ asset('assets/img/apple-touch-icon.png') }} rel="apple-touch-icon">
+    <link href={{ asset('assets/img/invlogo.png') }} rel="apple-touch-icon">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com" rel="preconnect">
